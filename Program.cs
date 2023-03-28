a426c8fc6709e9ba251e0129ea18d2676dc7a26e
sipra <sipramishra085@gmail.com>
2023-03-29 01:05:36 +0530

    Added Main file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day10AddressBook
+{
+    class Program
+    {
+
+        public string firstname, lastname, mobilenumber, address, city, state, zip, email;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to the Adress Book Program: ");
+
+
+        }
+    }
+}
