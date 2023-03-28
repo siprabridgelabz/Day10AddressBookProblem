e594ed2d5fbd329d63fce28d057e1d90169cfc1a
sipra <sipramishra085@gmail.com>
2023-03-29 01:12:22 +0530

    added UC1

---
+++ ContactManagementUC1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10AddressBook
+{
+    public class ContactManagementUC1
+    {
+        public static void CreateContact()
+        {
+            string firstname;
+            string lastname;
+            string address;
+            string city;
+            string state;
+            string zip;
+
+            string phone;
+            string email;
+            string phoneNumber;
+        }
+    }
+}
+
